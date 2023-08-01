7bb6daf466ad8ecb1d572cfeae1b1afa69d6ea1f
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2023-08-01 14:21:18 +0000

    aula array exercicio part 2

---
+++ aulaArrays.js
@@ -0,0 +1,21 @@
+// const meuArray = [10 , 20 , 30 , 40 , 50];
+// let segundoElemento = meuArray[1];
+// console.log(segundoElemento);
+
+const frutas = ['maça' , 'banana' , 'laranja' , 'uva' , 'manga'];
+// console.log(frutas.length);
+// frutas.push('pera');
+// frutas.splice(0, 1);
+// console.log(frutas);
+// console.log(meuArray.includes(25));
+// let a1=[1 , 2 , 3 , 4 , 5] , a2=[1 , 2 , 3 , 4 , 5];
+// console.log(a3 = a1.concat(a2));
+// const num4 = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+// numPar = [];
+// numPar = num4.filter(num4 => num4 % 2 === 0);
+// console.log(numPar);
+  
+// const num5 = [3 , 1 , 4 , 1 , 5 , 9 , 2 , 6 , 5 , 3 , 5];
+// console.log(num5.sort());
+console.log(frutas.indexOf('laranja'));
+
