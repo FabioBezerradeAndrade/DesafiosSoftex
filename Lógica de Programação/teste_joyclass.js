622a2dad8f538872b29bf366577d4b54f5fcf84a
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2023-08-07 13:31:12 +0000

    criado desafio 17 logica programacao no arquivo teste_joyclass

--- Lógica de Programação/teste_joyclass.js
+++ Lógica de Programação/teste_joyclass.js
@@ -11,17 +11,38 @@
 // console.log("Este é um tipo:")
 // console.log(typeof booleano)
 
-window.alert('programa imc')
-rodar = true;
-while (rodar){
-    try{
-        var peso = parseFloat(window.prompt('insira peso'));
-        var altura = parseFloat(window.prompt('insira altura'));
-        var imc = imc_function(peso, altura);
-        window.alert(imc);
-        rodar = false;
-    }catch(error){
-        window.alert('dados incorretos');
-    }
-}
-window.alert('finalizado');
+// window.alert('programa imc')
+// rodar = true;
+// while (rodar){
+//     try{
+//         var peso = parseFloat(window.prompt('insira peso'));
+//         var altura = parseFloat(window.prompt('insira altura'));
+//         var imc = imc_function(peso, altura);
+//         window.alert(imc);
+//         rodar = false;
+//     }catch(error){
+//         window.alert('dados incorretos');
+//     }
+// }
+// window.alert('finalizado');
+
+// Você deve criar três listas em JavaScript:
+
+// 1. para guardar os nomes de dez pessoas;
+// 2. para guardar as suas idades;
+// 3. para guardar as suas cores favoritas.
+
+// Em seguida, imprima essas listas. Depois, faça algumas modificações, alterando uma cor e uma idade. 
+//Ao terminar, imprima novamente todas as informações das três listas.
+
+
+let nome = ['Albert' , 'Bernardete' , 'Creuza' , 'Diego' , 'Estevão' , 'Fábio' ,  'Geraldo' , 'Heitor' , 'Igor' , 'Jeremias'];
+let idade = [15 , 20 , 17 , 68 , 55 , 60 , 45 , 74 , 99 , 7 ];
+let cor_favorita = ['azul' , 'verde' , 'amarela' , 'verde' , 'marrom' , 'preta' , 'laranja' , 'roxa' , 'azul' , 'branca' ];
+
+console.log(nome , idade , cor_favorita);
+
+idade.splice(8 , 1 , 47);
+cor_favorita.splice(1 , 1 , 'rosa');
+
+console.log(nome , idade , cor_favorita);
